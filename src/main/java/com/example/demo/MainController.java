b3bb2dad9cec6eca4e4a2b625329b1c2e02519b4
jms8793 <alstkd111@naver.com>
2024-02-28 16:01:05 +0900

    first commit

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	
+	@ResponseBody
+	@GetMapping("/sbb")
+	public String index() {
+		return "안녕하세요. 정민상입니다.";
+	}
+	
+}
